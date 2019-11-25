9adc7af11837405cbf5258c439709d935fd5e002
hetianrui <939822765@qq.com>
2019-11-25 17:31:00 +0800

    百万富翁问题\MillionMan.java

---
+++ 百万富翁问题/MillionMan.java
@@ -0,0 +1,9 @@
+package TestCode;
+
+public class MillionMan {
+    public static void main(String[] args) {
+      int a=300;
+      int b=1073741823;
+        System.out.println(a+" "+b);
+    }
+}
