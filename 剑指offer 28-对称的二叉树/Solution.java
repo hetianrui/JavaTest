681d2c5a320d25b9cd51725ae18ee6344e370b23
hetianrui <939822765@qq.com>
2020-08-03 22:42:12 +0800

    剑指offer 28-对称的二叉树\Solution.java

---
+++ 剑指offer 28-对称的二叉树/Solution.java
@@ -0,0 +1,34 @@
+
+
+
+ class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return mirror(root, root);
+    }
+
+    public boolean mirror(TreeNode l, TreeNode r) {
+        if (l == null && r != null) {
+            return false;
+        }
+        if (l != null && r == null) {
+            return false;
+        }
+        if (l == null && r == null) {
+            return true;
+        }
+        return (l.val == r.val) && mirror(l.left, r.right) && mirror(l.right, r.left);
+    }
+}
